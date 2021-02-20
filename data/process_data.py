558b48f2afd3b25341ba7310db6f62e4cadaface
Farai <conscious.f@gmail.com>
2021-02-20 12:33:15 +0200

    update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,82 @@
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
+def load_data(messages_filepath,categories_filepath):
+    """Function to load the data set.
 
-def load_data(messages_filepath, categories_filepath):
-    pass
+    Args:
+        messages_filepath: file path of the message csv
+        categories_filepath: file path of the categories csv
+
+    Returns:
+        df: merged dataframe
+
+    """
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    # merge datasets
+    df = pd.merge(messages, categories, on='id', how='left')
+
+    return df
 
 
 def clean_data(df):
-    pass
+    """Function to clean the data set.
 
+    Args:
+        df: dataframe to be cleaned
+
+    Returns:
+        df: clean dataframe
+
+    """
+
+
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(";",expand=True,)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[1]
+    category_colnames = row.str.split('-').str.get(0)
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.strip().str[-1]
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # drop the original categories column from `df`
+    df = df.drop(['categories'], axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], sort=False,axis=1)
+
+    # drop duplicates
+    df.drop_duplicates(subset="id", inplace=True)
+    df.dropna(inplace=True)
+
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    """Function to save the data set in a database.
+
+    Args:
+        df: dataframe to be saved
+        database_filename: location to save the database
+    Returns:
+        mome
+
+    """
+
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('Messages', engine, if_exists='replace', index=False)
 
 
 def main():
@@ -24,12 +90,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
